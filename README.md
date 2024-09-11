0574c5cb8721fc85200bca810012768921552755
Rodrigo Tenorio <rodrigo.tenorio@ligo.org>
2024-09-11 15:22:55 +0200

    Add basic implementation

---
+++ README.md
@@ -0,0 +1,21 @@
+# gw-foutstep
+
+Implements the method of [Valencia et al. arXiv:2406.16636](https://arxiv.org/abs/2406.16636)
+to compute Fourier transforms of discrete data with step-like behaviors.
+
+## Citing this work
+
+If this package has been useful for you work, please cite the following paper:
+```
+@article{Valencia:2024zhi,
+    author = "Valencia, Jorge and Tenorio, Rodrigo and Rossell\'o-Sastre, Maria and Husa, Sascha",
+    title = "{Mind the step: On the frequency-domain analysis of gravitational-wave memory waveforms}",
+    eprint = "2406.16636",
+    archivePrefix = "arXiv",
+    primaryClass = "gr-qc",
+    reportNumber = "LIGO-P2400263",
+    month = "6",
+    year = "2024",
+    journal="arXiv e-print",
+}
+```
---
+++ src/foutstep/stepdft.py
@@ -0,0 +1,89 @@
+import numpy as np
+
+
+def syss_dft(
+    timestamps: np.array,
+    td_samples: np.array,
+    dt: float,
+    sigmoid_loc: float = 0,
+    sigmoid_width: float = 10,
+):
+    """
+    EXPLAIN HERE THE FT CONVENTIONS.
+
+    Parameters
+    ----------
+    timestamps:
+        
+    td_samples:
+
+    dt:
+        Timestep. This is used to re-scale the FT.
+    sigmoid_loc:
+        
+    sigmoid_width:
+        
+
+    Returns
+    -------
+    frequencies:
+        
+    fd_samples:
+        Regularized DFT of td_samples 
+    """
+
+    regularizator = fit_sigmoid(td_samples, timestamps, sigmoid_loc, sigmoid_width)
+
+    residual = td_samples - regularizator
+    residual_fd = dt * np.fft.fft(residual)
+
+    frequencies = np.fft.fft(len(td_samples), dt)
+    fd_samples = (
+        residual_fd
+        + sigmoid_fd(frequencies, sigmoid_loc - timestamps[0], sigmoid_width),
+    )
+
+    return (frequencies, fd_samples)
+
+
+def fit_sigmoid(
+    td_samples: np.array,
+    timestamps: np.array,
+    sigmoid_loc: float,
+    sigmoid_width: float,
+):
+
+    one_zero_sigmoid = sigmoid(timestamps, loc=sigmoid_loc, width=sigmoid_width)
+
+    denominator = one_zero_sigmoid[-1] - one_zero_sigmoid[0]
+    fit_amplitude = (td_samples[-1] - td_samples[0]) / denominator
+    fit_offset = (
+        one_zero_sigmoid[-1] * td_samples[0] - one_zero_sigmoid[0] * td_samples[-1]
+    ) / denominator
+
+    return (
+        fit_amplitude * sigmoid(timestamps, loc=sigmoid_loc, width=sigmoid_width)
+        + fit_offset
+    )
+
+
+def sigmoid(
+    t: float,
+    loc: float,
+    width: float,
+):
+    return 0.5 * (np.tanh((t - loc) / width) + 1)
+
+
+def sigmoid_fd(f: float, loc: float, width: float):
+    """
+    Closed-form continuous FT of `sigmoid`.
+    See `syss_dft` for conventions.
+    """
+    return (
+        -0.5j
+        * np.pi
+        * width
+        * np.exp(-2j * np.pi * f * loc)
+        / np.sinh(np.pi**2 * f * width)
+    )
