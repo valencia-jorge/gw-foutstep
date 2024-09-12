00008a3bdae1da71db6af1b1e3d8a3aef734dbb6
Rodrigo Tenorio <rodrigo.tenorio@ligo.org>
2024-09-12 12:17:04 +0200

    Some cosmetic changes to the example

--- example.py
+++ example.py
@@ -1,44 +1,44 @@
-from foutstep.stepdft import syss_dft, fit_sigmoid, sigmoid_fd
+from foutstep.stepdft import syss_dft
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+# Create some mock data
 dt = 0.01
-timestamps = np.arange(-15, 15, dt)
-td_samples = np.tanh(timestamps) + 0.5 * np.exp(
-    -0.5 * ((timestamps + 1.5) / 2.0) ** 2
-) * np.sin(2.0 * timestamps)
+timestamps = -10 + dt * np.arange(2000)
 
-sigmoid_loc = 0.0
-sigmoid_width = 0.1
+osc_frequency = 2
 
-fd_samples, frequencies = syss_dft(
+td_samples = np.tanh(timestamps/0.5) + 0.5 * np.exp(
+    -0.5 * ((timestamps) / 0.5) ** 2
+) * np.sin(2 * np.pi * osc_frequency * timestamps)
+
+# Compute the FFT with regularisation!
+frequencies, fd_samples = syss_dft(
     td_samples,
     timestamps,
     dt,
-    sigmoid_loc,
-    sigmoid_width,
 )
 
+# Compute without regularisation for comparison...
+bare_fd_samples = dt * np.fft.fft(td_samples)
 
-regularizator = fit_sigmoid(
-    td_samples,
-    timestamps,
-    sigmoid_loc,
-    sigmoid_width,
-)
+# Plot!
+pos_freqs = slice(0, len(timestamps)//2)
 
-residual = td_samples - regularizator
-
-
-fig, axes = plt.subplots(1, 2)
+fig, axes = plt.subplots(2, 1, figsize=(16, 10))
+fig.suptitle("Simple application of foutstep")
+for ax in axes:
+    ax.grid(which="both")
+axes[0].set(xlabel="Timestamps [s]", ylabel="Time-domain signal")
+axes[1].set(xlabel="Frequencies [Hz]", ylabel=r"$\delta t \cdot $FFT", xscale="log", yscale="log")
 
 axes[0].plot(timestamps, td_samples)
-axes[0].plot(timestamps, regularizator, "--")
-axes[0].plot(timestamps, residual, "-.")
 
-axes[1].loglog(frequencies[frequencies > 0], np.abs(fd_samples[frequencies > 0]))
-axes[1].loglog(frequencies[frequencies>0], np.abs(sigmoid_fd(frequencies[frequencies>0], sigmoid_loc, sigmoid_width)), "--")
-axes[1].loglog(frequencies[frequencies>0], np.abs(dt*np.fft.fft(residual)[frequencies>0]), "-.")
-axes[1].set_ylim(ymax=10, ymin=1e-11)
+axes[1].plot(frequencies[pos_freqs], np.abs(bare_fd_samples[pos_freqs]), label="No regularisation")
+axes[1].plot(frequencies[pos_freqs], np.abs(fd_samples[pos_freqs]), label="SySS")
+axes[1].axvline(osc_frequency, color="red", label="Oscillation frequency", ls="--")
+
+axes[1].legend()
 
 plt.show()
