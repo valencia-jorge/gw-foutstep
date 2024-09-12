b45a80fbc0b95ec6062af76e39bfcdc500496d2a
Jorge Valencia <jorge.valencia@ligo.org>
2024-09-12 12:55:44 +0200

    add function descriptions

--- src/foutstep/stepdft.py
+++ src/foutstep/stepdft.py
@@ -9,27 +9,35 @@ def syss_dft(
     sigmoid_width: float = 10,
 ):
     """
-    EXPLAIN HERE THE FT CONVENTIONS.
-
-    Parameters
-    ----------
+    This function computes the Fourier transform of discrete data with step-like behaviors as described in [Valencia et al. (arXiv:2406.16636)].
+    The Fourier convention follows Eq.(2.16).
+    An oscillatory factor corresponding to a timeshift of timestamps[0] is taken into account following Eqs.(2.12)-(2.15).
+    
     timestamps:
+        time-coordinates at of the data points
 
     td_samples:
+        y-coordinates of the data points
 
     dt:
-        Timestep. This is used to re-scale the FT.
+        timestep used to re-scale the discrete Fourier transform
+
     sigmoid_loc:
-
+        time coordinate at which the regularizing sigmoid is centered.
+        Corresponds to t_jump in Eq.(3.6)
+        
     sigmoid_width:
-
+        spreadness of the regularizing sigmoid
+        Corresponds to sigma in Eq.(3.6)
 
     Returns
-    -------
-    frequencies:
-
+    ------------
     fd_samples:
-        Regularized DFT of td_samples
+        Regularized Discrete Fourier Transform of td_samples.
+
+    frequencies:
+        Discrete Fourier Transform sample frequencies at which fd_samples is evaluated.
+
     """
 
     regularizator = fit_sigmoid(td_samples, timestamps, sigmoid_loc, sigmoid_width)
@@ -40,7 +48,7 @@ def syss_dft(
     frequencies = np.fft.fftfreq(len(td_samples), dt)
     fd_samples = residual_fd + sigmoid_fd(
         frequencies, sigmoid_loc - timestamps[0], sigmoid_width
-    )
+    ) # Eq.(3.7)
 
     return (frequencies, fd_samples)
 
@@ -51,6 +59,31 @@ def fit_sigmoid(
     sigmoid_loc: float,
     sigmoid_width: float,
 ):
+    """
+    This function fits the regularizing sigmoid to the discrete dataset according to Eq.(3.6).
+    
+
+    Parameters
+    ------------
+    td_samples:
+        y-coordinates of the data points
+
+    timestamps:
+        time-coordinates at which td_samples is evaluated
+
+    sigmoid_loc:
+        time coordinate at which the regularizing sigmoid is centered.
+        Corresponds to t_{jump} in Eq.(3.6)
+        
+    sigmoid_width:
+        spreadness of the regularizing sigmoid
+        Corresponds to sigma in Eq.(3.6)
+
+    Returns
+    ------------
+        y-coordinates of the regularizing sigmoid.
+        
+    """
 
     one_zero_sigmoid = sigmoid(timestamps, loc=sigmoid_loc, width=sigmoid_width)
 
@@ -71,13 +104,49 @@ def sigmoid(
     loc: float,
     width: float,
 ):
+    """
+    This function defines a regularizing sigmoid with amplitude 1 and offset 0.
+    
+
+    Parameters
+    ------------
+    t:
+        time-coordinates at which the sigmoid is evaluated.
+
+    loc:
+        time coordinate at which the sigmoid is centered.
+        
+    width:
+        spreadness of the sigmoid.
+
+    Returns
+    ------------
+        y-coordinates of the sigmoid.
+        
+    """
     return 0.5 * (np.tanh((t - loc) / width) + 1)
 
 
 def sigmoid_fd(f: float, loc: float, width: float):
     """
-    Closed-form continuous FT of `sigmoid`.
-    See `syss_dft` for conventions.
+    Closed-form continuous Fourier transform of `sigmoid` following the conventions of Eq.(2.1)
+    
+
+    Parameters
+    ------------
+    f:
+        frequency sampples.
+
+    loc:
+        time coordinate at which the sigmoid is centered.
+        
+    width:
+        spreadness of the sigmoid
+
+    Returns
+    ------------
+        y-coordinates of the Fourier transform of `sigmoid`
+
     """
     return (
         -0.5j
